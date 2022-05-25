d885797f0d5bf6c4db39b6212684a95f7cb8db16
brianfu <fubof@sfu.ca>
2022-05-25 02:58:46 -0700

    fixed gitignore, added workaround for unstable dev environment

---
+++ open_vscode_setup.sh
@@ -0,0 +1,19 @@
+cd ~/sfuhome/code/SAGA-Bench_CUDA
+#Use: Right click -> 'Copy relative path'
+
+# Standard
+code open_vscode_setup.sh
+code README.md
+code runme.sh
+code .vscode/launch.json
+code Makefile
+# code .gitignore
+
+code src/dynamic/frontEnd.cu
+code src/dynamic/builder.cu
+
+# BFSdyn testing
+code src/dynamic/dyn_bfs_cu.h
+code src/dynamic/topAlg_cu.h
+code src/dynamic/adList_cu.h
+code src/dynamic/adList_cu_support.h
